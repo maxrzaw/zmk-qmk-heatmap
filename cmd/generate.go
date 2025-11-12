9a6dcefd393d87589b878255ab1c9df37e61f7d5
Max Zawisa <maxrzaw@umich.edu>
2025-11-11 23:35:55 -0500

    Add dark mode

--- cmd/generate.go
+++ cmd/generate.go
@@ -21,6 +21,7 @@ func init() {
 	generateCmd.Flags().StringVarP(&inputParam, "input", "i", "heatmap.json", "e.g. ~/heatmap.json")
 	generateCmd.Flags().StringVarP(&keymapParam, "keymap", "m", "keymap.yaml", "e.g. ~/keymap.yaml")
 	generateCmd.Flags().IntVarP(&numSensors, "sensors", "s", 0, "the number of sensors (encoders) of the keyboard, this value is needed to properly collect combos")
+	generateCmd.Flags().Bool("dark", false, "darkmode")
 }
 
 var generateCmd = &cobra.Command{
@@ -30,6 +31,8 @@ var generateCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// Remove the timestamp from the log messages
 		log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+
+		darkMode, _ := cmd.Flags().GetBool("dark")
 
 		heatmap, err := heatmappkg.Load(inputParam)
 		if err != nil {
@@ -63,10 +66,20 @@ var generateCmd = &cobra.Command{
 		}
 		log.Println("Loading keymap", keymapFile)
 
+		if darkMode {
+			fmt.Printf("svg { background-color: #0e1117; }\nrect.key { fill: #131419; }\n")
+		}
+
 		for _, press := range heatmap.KeyPresses {
 			perc := (float32(press.GetTotalPressCounts()) / float32(max))
 			luminance := uint8(255 * perc)
-			color := heatmappkg.RgbaToRgb(color.RGBA{R: 255, G: 0, B: 0, A: luminance}, heatmappkg.RGB{R: 255, G: 255, B: 255})
+
+			backgroundColor := heatmappkg.RGB{R: 255, G: 255, B: 255}
+			if darkMode {
+				backgroundColor = heatmappkg.RGB{R: 19, G: 20, B: 25}
+			}
+
+			color := heatmappkg.RgbaToRgb(color.RGBA{R: 255, G: 0, B: 0, A: luminance}, backgroundColor)
 
 			/*
 					as an alternative you can do this, which draws a border around the text, this is useful when you want to
@@ -89,7 +102,12 @@ var generateCmd = &cobra.Command{
 
 			perc := (float32(press.GetTotalPressCounts()) / float32(max))
 			luminance := uint8(255 * perc)
-			color := heatmappkg.RgbaToRgb(color.RGBA{R: 255, G: 0, B: 0, A: luminance}, heatmappkg.RGB{R: 255, G: 255, B: 255})
+			backgroundColor := heatmappkg.RGB{R: 255, G: 255, B: 255}
+			if darkMode {
+				backgroundColor = heatmappkg.RGB{R: 19, G: 20, B: 25}
+			}
+
+			color := heatmappkg.RgbaToRgb(color.RGBA{R: 255, G: 0, B: 0, A: luminance}, backgroundColor)
 			fmt.Printf("svg > g:nth-of-type(%d) .combopos-%d rect { stroke: #c9cccf; stroke-width: 1; fill: #%02X%02X%02X; }\n", press.Layer+1, comboIndex, color.R, color.G, color.B)
 		}
 	},
